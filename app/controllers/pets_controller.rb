6f80d436922a50581b885de2a1be763f40935300
Alex Wilson <fonzmp@gmail.com>
2018-06-18 19:29:35 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,8 +13,8 @@ class PetsController < ApplicationController
   post '/pets' do
     @pet = Pet.create(params[:pet])
     if !params[:owner][:name].emtpy?
-      @owner = Owner.find(params[:pet][:owner_id])
-      @pet.owner_id << @owner.id
+        @owner = Owner.create(params[:owner][:name])
+        @pet.owner_id = @owner.id
     end
     binding.pry
   end
