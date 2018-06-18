781fa902adcb7a1966939f5d35a7d24702431e4a
Alex Wilson <fonzmp@gmail.com>
2018-06-18 19:49:34 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -27,7 +27,7 @@ class PetsController < ApplicationController
   post '/pets/:id' do
     @pet = Pet.find(params[:id])
     @pet.update(params[:pet])
-    if !params[]
+    binding.pry
 
     redirect to "pets/#{@pet.id}"
   end
