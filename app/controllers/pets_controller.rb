8a7c6428b1ac596b4c71db180cb5f3f7cf800932
Alex Wilson <fonzmp@gmail.com>
2018-06-18 19:52:29 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -27,7 +27,7 @@ class PetsController < ApplicationController
   post '/pets/:id' do
     @pet = Pet.find(params[:id])
     @pet.update(params[:pet])
-    if !params[:owner][:name].emtpy?
+    if !params[:owner][:name].empty?
       @pet.owner = Owner.create(params[:owner])
     end
     binding.pry
