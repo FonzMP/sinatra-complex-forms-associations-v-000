debeb92ec5d8a946f8d00224ad7647198c2d86bd
Alex Wilson <fonzmp@gmail.com>
2018-06-18 19:47:14 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -27,6 +27,9 @@ class PetsController < ApplicationController
   post '/pets/:id' do
     @pet = Pet.find(params[:id])
     @pet.update(params[:pet])
+    if !params[:owner][:name].empty?
+
+    end
 
     redirect to "pets/#{@pet.id}"
   end
